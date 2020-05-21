6261f5bc56bb25ad413ed966d3d8d461d1a61c29
Nkansah <nkansahalbert.code@gmail.com>
2020-05-21 14:56:35 +0000

    add this folder

---
+++ JAVA_PROG/Static modifier /Static_block.java
@@ -0,0 +1,10 @@
+class Static_block{
+  static{
+    System.out.println("Static block is invoked");
+  }
+  public static void main(String[] args){
+    System.out.println("The main method is invoked");
+  }
+
+
+}
---
+++ JAVA_PROG/this_Keyword/TestThis2.java
@@ -0,0 +1,27 @@
+
+class Student{
+  int rollno;
+  String name;
+  float fee;
+  Student(int rollno,String name,float fee){
+    this.rollno = rollno; //the this keyword in class methods
+    this.name = name;
+    this.fee = fee;
+
+  }
+  void display(){
+    System.out.println(rollno+" "+name+" "+fee);
+  }
+}
+
+class TestThis2{
+  public static void main(String[] args){
+
+    Student s1 = new Student(111,"Abena",5000f);
+    Student s2 = new Student(112,"Nkansah",6000f);
+
+    s1.display();
+    s2.display();
+    
+  }
+}
