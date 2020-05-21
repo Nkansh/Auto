4ced1bdcc9bcb4b2a2f4ac87491ff2d17be31473
Nkansah <nkansahalbert.code@gmail.com>
2020-05-21 02:01:00 +0000

    add A.java

---
+++ JAVA_PROG/Static modifier /A.java
@@ -0,0 +1,8 @@
+class A{
+  static int a;
+  static boolean x;  
+  public static void main(String[] args){
+    System.out.println(a); //output default for int(0)
+     System.out.println(x); //output default for boolean                            (false)
+  }
+}
